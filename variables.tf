c27b584fa34d04849f0620c5beee867480e6d7a3
Aliu Ede <111136362+Owolabi16@users.noreply.github.com>
2025-03-21 12:03:31 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "proxy_url" {
+  description = "URL to proxy requests to (required)"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t4g.medium"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "ami" {
+  description = "Amazon Linux 2 AMI ID"
+  type        = string
+  default     = "ami-08b5b3a93ed654d19"
+}
